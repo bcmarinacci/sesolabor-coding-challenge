cf0efae7c51980d9c41a15167aa47b931590a4c8
Benji Marinacci <bcmarinacci@icloud.com>
2023-10-25 23:43:31 -0700

    Complete sync sort

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,32 @@
 "use strict";
 
+const { MinHeap } = require("data-structure-typed");
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const minHeap = new MinHeap((a, b) => a.log.date - b.log.date);
+
+  // Seed min heap with first log from each source
+  for (let i = 0; i < logSources.length; i++) {
+    const log = logSources[i].pop();
+    if (log) {
+      minHeap.push({ log, sourceIndex: i });
+    }
+  }
+
+  while (minHeap.size > 0) {
+    const oldest = minHeap.pop();
+    printer.print(oldest.log);
+
+    const oldestIndex = oldest.sourceIndex;
+    const nextLog = logSources[oldestIndex].pop();
+
+    // Add a log to the min heap, if one exists, otherwise decrement the number of unemptied log sources
+    if (nextLog) {
+      minHeap.push({ log: nextLog, sourceIndex: oldestIndex });
+    }
+  }
+
+  printer.done();
 };
